218a4c452e682ee6cf7d497867dbbb8e9bf6d8d5
mathxlee <liyi2@yy.com>
2019-01-09 13:03:02 +0800

    update babel transform problem

--- src/devices/ios.js
+++ src/devices/ios.js
@@ -1,27 +1,25 @@
 import DeviceBasic from './basic';
 
+let devices;
+
 //获取IOS的机型
 export default class DeviceIos extends DeviceBasic {
     constructor() {
-        () => {
-            this.setDevices();
-        }
-    }
+        super();
 
-    setDevices() {
-        this.devices = {
+        devices = {
             "Apple A7 GPU": {
                 1136: ["iPhone 5", "iPhone 5s"],
                 2048: ["iPad Air", "iPad Mini 2", "iPad Mini 3"]
             },
-
+        
             "Apple A8 GPU": {
                 1136: ["iPod touch (6th generation)"],
                 1334: ["iPhone 6"],
                 2001: ["iPhone 6 Plus"],
                 2048: ["iPad Air 2", "iPad Mini 4"]
             },
-
+        
             "Apple A9 GPU": {
                 1136: ["iPhone SE"],
                 1334: ["iPhone 6s"],
@@ -29,25 +27,26 @@ export default class DeviceIos extends DeviceBasic {
                 2224: ["iPad Pro (9.7-inch)"],
                 2732: ["iPad Pro (12.9-inch)"]
             },
-
+        
             "Apple A10 GPU": {
                 1334: ["iPhone 7"],
                 2001: ["iPhone 7 Plus"]
             },
-
+        
             "Apple A11 GPU": {
                 1334: ["iPhone 8"],
                 2208: ["iPhone 8 Plus"],
                 2436: ["iPhone X"],
             },
-
+        
             "Apple A12 GPU": {
                 1792: ["iPhone Xr"],
                 2436: ["iPhone Xs"],
                 2688: ["iPhone Xs Max"],
             }
-        }
+        };
     }
+
     getGlRenderer() {
         try {
             let canvas = document.createElement('canvas');
@@ -59,10 +58,11 @@ export default class DeviceIos extends DeviceBasic {
             return ''
         }
     }
+
     getPhoneType(GPU) {
         let height = this.getHeight()
         GPU = GPU || this.getGlRenderer()
-        let device = this.devices[GPU]
+        let device = devices[GPU]
         if (!device) return ''
         return device[height][0] ? device[height][0] : ''
     }
